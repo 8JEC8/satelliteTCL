07b1bdb8b70f337566b27f40cb2bba16c868a77c
8JEC8 <ecastellanos220@gmail.com>
2025-04-07 20:16:21 -0600

    Update boot.py
    Temp y Gyro funcionando (Falta INA219)

--- ap/boot.py
+++ ap/boot.py
@@ -12,78 +12,66 @@ connection_active = False  # Flag, True=Conn activa, False=Conn desactivada
 conn = None  # Será global
 server_socket = None  # Será global
 
-# Abrir/Crear log, sobreescribir
-log_file = open("ap_log.txt", "w")
+log_file = open("ap_log.txt", "w") # Abrir log
 
-######################## Funciones del Programa
 def log_message(message):
     t = time.localtime()
-    timestamp = "[04/04/2025]" + "[{:02d}:{:02d}:{:02d}]".format(*t[3:6])  # Fecha manual, T en 00:00:00 (2 Digitos en tiempo)
-
+    timestamp = "[06/04/2025]" + "[{:02d}:{:02d}:{:02d}]".format(*t[3:6])  # Fecha manual, T en 00:00:00 (2 Digitos en tiempo)
     formatted_message = f"{timestamp} {message}"
-
     sys.stdout.write(formatted_message + "\n")
     log_file.write(formatted_message + "\n")
     log_file.flush()  # Escribir inmediatamente
 
 def receive_messages():
+    global conn, connection_active
+
     while True:
-        if not connection_active:
+        if not connection_active: #Si conexión activa, try; Si conexión no activa, reiniciar while True
             continue
-          
+
         try:
             data = conn.recv(1024).decode()
             if not data:
                 break
-              
-            # Log received message from STA
             log_message(f"Satélite (STA): {data}")
-        
+
         except OSError:
-            log_message("    Mensaje no reconocido por Satélite (STA), se confirma cerrado de Socket")
+            log_message("Desconexión detectada. Reiniciando socket...")
             break
-          
+
         except Exception as e:
-            log_message(f"    Error recibiendo mensaje de Satélite (STA): {e}")
+            log_message(f"Error recibiendo mensaje de Satélite (STA): {e}")
             break
 
-def connect_socket():
+    # Reset de conn, intentar conexión otra vez
+    if conn:
+        conn.close()
+        conn = None
+
+    connection_active = False
+    connection_loop() 
+
+def connection_loop():
     global conn, server_socket, connection_active
+    connection_active = False
+    try:
+        if server_socket:
+            server_socket.close()
+    except:
+        pass
 
-    if connection_active:
-        log_message("La conexión ya existe")
-        return
-
-    if server_socket:
-        server_socket.close()  # Cerrar server socket si ya está creado
-    
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind(('0.0.0.0', 1234))
     server_socket.listen(1)
     log_message("AP (You: Est. Terrestre) Esperando a STA (Satélite)...")
-  
+
     conn, addr = server_socket.accept()
     connection_active = True
     log_message(f"Conectado a {addr}")
-
-    _thread.start_new_thread(receive_messages, ()) #Al conectar socket, recepción simultanea
-
-def disconnect_socket():
-    global conn, connection_active, server_socket
-
-    if conn:
-        conn.close()
-        conn = None
-        log_message("Socket Cerrado: Est. Terrestre (AP)")
-    else:
-        log_message("La conexión ya está cerrada")
-    
-    if server_socket:
-        server_socket.close()
-        server_socket = None
-    
-    connection_active = False
+    log_message("LISTA DE COMANDOS: command.list")
+    sys.stdout.write("\n")
+    _thread.start_new_thread(receive_messages, ())
 
 ######################## Setup de AP
 ap = network.WLAN(network.AP_IF)  # Crear AP
@@ -94,35 +82,35 @@ sys.stdout.write("\n\n")
 log_message("AP activado")
 log_message(f"SSID: {ssid}")
 log_message(f"IP Address: {ap.ifconfig()[0]}")
-log_message("LISTA DE COMANDOS: command.list")
+
+connection_loop()  # Iniciar escuchando la primera conexión
 
 ######################## Envio de Mensajes
+
 while True:
     is_sending = 1  # Enviando mensaje
     message = input("")
     if message == "":
         log_message("TERMINAL_ERROR: Mensaje vacío")
         continue
+    
     elif message == "command.list":
-        log_message("'sat.CON'     : Conectar con STA")
-        log_message("'sat.DC'      : Desconectar de STA")
+        log_message(message)
         log_message("'sat.RSSI'    : Desplegar valor de RSSI")
         log_message("'sat.LED ON'  : Encender LED")
         log_message("'sat.LED OFF' : Apagar LED")
-        continue
-    elif message == "sat.CON":
-        connect_socket()
-        continue
-    elif message == "sat.DC":
-        disconnect_socket()
+        log_message("'sat.TEMP'    : Temperatura y Humedad")
+        log_message("'sat.GYRO'    : Acelerómetro y Giroscopio")
         continue
 
     if not connection_active:
         log_message("¡NO CONEXIÓN! El mensaje no fue enviado.")
-        continue  # Reiniciar while True
-          
-    conn.send(message.encode())
-    log_message(f"Est. Terrestre (AP): {message}")
-          
-    conn.send(message.encode())
-    log_message(f"Est. Terrestre (AP): {message}")
+        continue
+
+    try:
+        conn.send(message.encode())
+        log_message(f"Est. Terrestre (AP): {message}")
+    except Exception as e:
+        log_message(f"Error al enviar mensaje: {e}")
+        connection_active = False
+        conn = None
