6412d6f374747bbf9126db1c38801f11624c5fa5
Rodrigo Gamboa <rodrigo.gamboa@Outlook.com>
2025-04-13 18:23:38 -0600

    fix: syntax error

--- lib/logger.py
+++ lib/logger.py
@@ -8,5 +8,5 @@ class Logger:
     def info(self, message):
         # [DD/MM/YY][HH:MM:SS][Name/INFO]: MSG
         t = time.gmtime()
-        print(f'[{t[2]:02}/{t[1]:02}/{t[0]}][{t[3]:02}:{t[4]:02}:{t[5]:02}][{self.name}/INFO]: {message}'))
+        print(f'[{t[2]:02}/{t[1]:02}/{t[0]}][{t[3]:02}:{t[4]:02}:{t[5]:02}][{self.name}/INFO]: {message}')
         # not writing yet to not waste space
