3f95e38deb33e5e097fd01bd2764accc88eeace6
Rodrigo Gamboa <rodrigo.gamboa@Outlook.com>
2025-05-05 21:25:08 -0600

    feat: more logger levels

--- lib/logger.py
+++ lib/logger.py
@@ -5,8 +5,21 @@ class Logger:
         self.file = open(filedir, "w")
         self.name = name
 
-    def info(self, message):
-        # [DD/MM/YY][HH:MM:SS][Name/INFO]: MSG
+    def __print(self, message, level_prefix):
         t = time.gmtime()
-        print(f'[{t[2]:02}/{t[1]:02}/{t[0]}][{t[3]:02}:{t[4]:02}:{t[5]:02}][{self.name}/INFO]: {message}')
+        # [DD/MM/YY][HH:MM:SS][Name/INFO]: MSG
+        print(f'[{t[2]:02}/{t[1]:02}/{t[0]}][{t[3]:02}:{t[4]:02}:{t[5]:02}][{self.name}/{level_prefix}]: {message}')
         # not writing yet to not waste space
+
+    def info(self, message):
+        self.__print(message, 'INFO')
+
+    def warn(self, message):
+        self.__print(message, 'WARN')
+
+    def debug(self, message):
+        if False:
+            self.__print(message, 'DEBUG')
+
+    def error(self, message):
+        self.__print(message, 'ERROR')
