b1dc3325286821a2b8728dd379f07a53ff7fb871
Rodrigo Gamboa <rodrigo.gamboa@outlook.com>
2025-06-06 19:15:31 -0600

    fix: remove unsopported led feature

--- lib/led.py
+++ lib/led.py
@@ -1,36 +1,37 @@
-import machine, neopixel
+#import machine, neopixel
 
 class Led:
     def __init__(self):
         self.color = [0, 0, 0]
         self.active = False
-        self.led = neopixel.NeoPixel(machine.Pin(38), 1)
-        self.led[0] = self.color
-        self.led.write()
+        #self.led = neopixel.NeoPixel(machine.Pin(38), 1)
+        #self.led[0] = self.color
+        #self.led.write()
         
     def on(self):
         self.active = True
-        self.led[0] = self.color
-        self.led.write()
+        #self.led[0] = self.color
+        #self.led.write()
 
     def off(self):
         self.active = False
-        self.led[0] = [0, 0 ,0]
-        self.led.write()
+        #self.led[0] = [0, 0 ,0]
+        #self.led.write()
     
     def red(self):
-        self.color = [2, 0, 0]
+        #self.color = [2, 0, 0]
+        pass
 
     def green(self):
-        self.color = [0, 2, 0]
+        #self.color = [0, 2, 0]
+        pass
 
     def amber(self):
-        self.color = [2, 1, 0]
+        #self.color = [2, 1, 0]
+        pass
 
     def toggle(self):
         if self.active:
             self.off()
-            self.active = False
         else:
             self.on()
-            self.active = True
