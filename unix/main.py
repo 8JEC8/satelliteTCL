4460dac778e0b2798b48c3bd5294ba9194ffe8c7
Rodrigo Gamboa <rodrigo.gamboa@Outlook.com>
2025-06-08 21:48:01 -0600

    draft: compat changes 2. autosend

--- unix/main.py
+++ unix/main.py
@@ -1,13 +1,26 @@
 from commander import Commander
 from peer_tcp import Peer
 from sock import Socker
+from logger import Logger
 
 socket = Socker()
-
-Peer.DEFAULT_EXT_ID = 'rodro'  # our identity
-socket.peers["earth"] = Peer(('172.16.20.157', 8081), "earth", 0, None, outbound=True)
-
 commands = Commander(socket)
-commands.masters.append('earth')
 commands._refresh()
 
+Peer.DEFAULT_EXT_ID = 'rodro'  # our identity
+
+def setExtId(id):
+    Peer.DEFAULT_EXT_ID = id
+
+def connectTo(refName, host, port): # string, string, int
+    socket.peers[refName] = Peer((host, port), refName, 0, None, outbound=True)
+    commands.masters.append(refName)
+
+def setPrinter(func):
+    print(f'Reassigning printer to {func}')
+    Logger.Printer = func
+
+def getPhysicalStatus():
+    #return ((25, 80), (3.3, 77), (13.2, 13.3, 13.4), (-10,))
+    return commands.readStatus()
+    # (temp/hum), (volt, mAmp), (xdeg, ydeg, zdeg), (rssi)
